a6dfa88e11027ede2e88d81bc1ec3439ca988d8b
mr-z-one <mr.z.0x0001@gmail.com>
2025-11-16 01:20:49 +0330

    finishing challenge 1,2,3

---
+++ level-01/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--level-01-->
+<html lang="fa" dir="rtl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>challenge</title>
+    <link rel="stylesheet" href="/bootstrap-5.3.8-dist/css/bootstrap.rtl.min.css">
+    <link rel="stylesheet" href="/style.css">
+</head>
+
+<body>
+
+
+        <section id="video-background">
+               <video  autoplay muted loop src="/static/1.mp4"></video>
+               <div class="container-fluid">
+                  <div class="row">
+                    <div class="col d-flex justify-content-center">
+                        <p class="text-white">
+                    می توانید به مرحله بعد بروید؟
+                    </p>
+                    </div>
+                  </div>
+               </div>
+        </section>
+     
+    
+
+    <script src="/bootstrap-5.3.8-dist/js/bootstrap.bundle.js"></script>
+</body>
+
+</html>
---
+++ level-02/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<!--level-01-->
+<html lang="fa" dir="rtl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>challenge</title>
+    <link rel="stylesheet" href="/bootstrap-5.3.8-dist/css/bootstrap.rtl.min.css">
+    <link rel="stylesheet" href="/style.css">
+</head>
+
+<body>
+
+
+        <section id="video-background">
+               <video  autoplay muted loop src="/static/2.mp4"></video>
+               <div class="container-fluid">
+                  <div class="row">
+                    <div class="col d-flex justify-content-center">
+                        <p class="text-white">
+                    گاهی حقیقت در جلوی چشم هایتان پنهان می شود!
+                    </p>
+                    </div>
+                  </div>
+               </div>
+        </section>
+     
+    
+
+    <script src="/bootstrap-5.3.8-dist/js/bootstrap.bundle.js"></script>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- /aX13vfHl9N -->
